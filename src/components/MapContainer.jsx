03df948a6caf83e2ea4a1573021a1d33f2c5e0a1
lolololol-21 <216166682+lolololol-21@users.noreply.github.com>
2026-03-08 09:14:03 +0800

    Fix 401 error by switching to free CartoDB map tiles

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -47,8 +47,8 @@ const MapContainer = ({ sites, onSiteSelect, selectedSite }) => {
       >
         {/* Using a beautiful, clean map tile */}
         <TileLayer
-          attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         
         {sites.map((site) => (
